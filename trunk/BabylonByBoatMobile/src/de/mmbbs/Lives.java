5dbef3c27a59dbd3a43afd802ef83f34f10760be
khakbunn@gmail.com <khakbunn@gmail.com@ac8687c6-834d-e4e5-98ed-2d5b16657450>
2012-02-16 13:59:48 +0000

    Unnötiger Code entfernt

--- trunk/BabylonByBoatMobile/src/de/mmbbs/Lives.java
+++ trunk/BabylonByBoatMobile/src/de/mmbbs/Lives.java
@@ -13,9 +13,6 @@ public class Lives {
 
 	// Leben zeichnen
 	public void paint (Canvas g, Paint p) {
-		Paint paint = new Paint();
-	    paint.setStyle(Paint.Style.FILL);
-
 		g.drawText("Lives: " + livecounter.toString(), 10, 10, p);
 	}
 
