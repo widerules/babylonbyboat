11c90aa6881010413312fc1486df8a49686018cb
alexander.maier@dialog-edv.de <alexander.maier@dialog-edv.de@ac8687c6-834d-e4e5-98ed-2d5b16657450>
2012-02-16 17:49:04 +0000

    Klasse ExplosionListener implementiert Dummy-Methoden (ohne Inhalt) hinzugefügt

--- trunk/BabylonByBoatMobile/src/de/mmbbs/GameCanvas.java
+++ trunk/BabylonByBoatMobile/src/de/mmbbs/GameCanvas.java
@@ -1,4 +1,5 @@
 package de.mmbbs;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -6,28 +7,81 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.SurfaceView;
 
-public class GameCanvas extends SurfaceView  {
+/**
+ * 
+ * (16.02.2012 14:58:53) am:<ul><li>Neue Methoden hinzugefügt (ohne Inhalt).</li></ul>
+ * (16.02.2012 14:43:54) am:<ul><li>Klasse ExplosionListener wird nun implementiert</li></ul>
+ * 
+ * @author Alex
+ *
+ */
+public class GameCanvas extends SurfaceView implements ExplosionListener {
 
-	public GameCanvas(Context context) {
-		super(context);
-	}
-	
-	public GameCanvas(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    // TODO-: (am-16.02.2012): States über StatePattern auslagern!
+    private static final int STATE_LOSTLIVE = 1;
+    private static final int STATE_SPIEL = 1;
+    private static final int STATE_GAMEOVER = -1;
 
-	@Override
-	protected void onDraw(Canvas g) {
-		// TODO Auto-generated method stub
-		super.onDraw(g);
-		this.render(g);
-	}
+    private int lives;
+    private int gameState;
 
-	public void render(Canvas c) {
-		// TODO Auto-generated method stub
-		Paint p = new Paint();
-		p.setColor(Color.CYAN);
-		c.drawText("Hello Game", 100, 10, p);
-	}
-	
+    public GameCanvas(Context context) {
+        super(context);
+    }
+
+    public GameCanvas(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas g) {
+        // TODO Auto-generated method stub
+        super.onDraw(g);
+        this.render(g);
+    }
+
+    public void render(Canvas c) {
+        // TODO Auto-generated method stub
+        Paint p = new Paint();
+        p.setColor(Color.CYAN);
+        c.drawText("Hello Game", 100, 10, p);
+    }
+
+    /**
+     * getLives - Liefert die Anzahl der Lebenspunkte.
+     * 
+     * @return - Anzahl Leben
+     */
+    public int getLives() {
+        return this.lives;
+    }
+
+    /**
+     * setLives - Setzt die Anzahl Leben
+     * 
+     * @param lives - Anzahl Leben
+     */
+    private void setLives(int lives) {
+        this.lives = lives;
+    }
+
+    private void run() {
+        // TODO-: (am-16.02.2012): noch mit Inhalt füllen
+    }
+
+    /**
+     * getState - Liefert den aktuellen Spielstatus (state)
+     */
+    public void geState() {
+        // TODO-: (am-16.02.2012): noch mit Inhalt füllen
+    }
+
+    /**
+     * decLives - Zieht den aktuellen Lebenspunkten einen Lebenspunkt ab.
+     */
+    private void decLives() {
+        // TODO-: (am-16.02.2012): noch mit Inhalt füllen
+        setLives(getLives() - 1);
+    }
+
 }
