285049a7742387c76b416d136bb0dc6c4eda8411
khakbunn <khakbunn@googlemail.com>
2012-02-23 14:00:15 +0000

    Lives Klasse erweitert (noch nicht fertig) - ein Leben wird angezeigt

--- BabylonByBoatMobile/src/de/mmbbs/Lives.java
+++ BabylonByBoatMobile/src/de/mmbbs/Lives.java
@@ -1,19 +1,36 @@
 package de.mmbbs;
 
+import java.io.InputStream;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 
 public class Lives {
 	private Integer livecounter;
+	private Bitmap bm;
 
 	// Konstruktor
-	public Lives () {
+	public Lives (Context context) {
+		InputStream is = context.getResources().openRawResource(R.drawable.herz);
+		BitmapFactory.Options opts = new BitmapFactory.Options();
+		opts.inJustDecodeBounds = true;
+		this.bm = BitmapFactory.decodeStream(is);
+
 		this.livecounter = 3;
 	}
 
 	// Leben zeichnen
-	public void paint (Canvas g, Paint p) {
+	@Deprecated
+	public void paint_alt (Canvas g, Paint p) {
 		g.drawText("Lives: " + livecounter.toString(), 10, 10, p);
+	}
+
+	public void paint (Canvas g, Paint p) {
+		g.drawBitmap(this.bm, 10, 10, p);
 	}
 
 	// Leben um 1 reduzieren
