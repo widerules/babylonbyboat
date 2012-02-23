f91513b1aa946ea19f400bf671d9ff0b302ceb4d
darkstaple.ggmn@googlemail.com <darkstaple.ggmn@googlemail.com@ac8687c6-834d-e4e5-98ed-2d5b16657450>
2012-02-23 11:23:57 +0000

    Klassenlayout bzw. Aufbau erstellt, leere Funktionen hinzugefügt

--- trunk/BabylonByBoatMobile/src/de/mmbbs/Basic2dObject.java
+++ trunk/BabylonByBoatMobile/src/de/mmbbs/Basic2dObject.java
@@ -1,11 +1,41 @@
 package de.mmbbs;
 
 import android.graphics.Canvas;
+import android.provider.MediaStore.Images;
 
 public class Basic2dObject {
+	
+	private Images image;
+	private int xPos;
+	private int yPos;
 
 	public Basic2dObject(String ressourcename) {
 		
+	}
+	
+	public int getWidth()
+	{
+		return 0;
+	}
+	
+	public int getHeight()
+	{
+		return 0;
+	}
+	
+	public boolean hit(int O)
+	{
+		return true; 
+	}
+	
+	public int getXPos()
+	{
+		return 0;
+	}
+	
+	public int getYPos()
+	{
+		return 0;
 	}
 	
 	public void paint(Canvas c) {
