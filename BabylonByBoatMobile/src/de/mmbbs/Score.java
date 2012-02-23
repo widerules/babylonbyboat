2a79c1a234cccef9154878f4a70fe04676429fef
hauke.muenstermann <hauke.muenstermann@googlemail.com>
2012-02-23 14:04:56 +0000

    Klasse Score erstellt. - Konstruktor setzt Punkte auf 0 - Paint Methode gibt Punkte rechts in der Titelleiste aus

---
+++ BabylonByBoatMobile/src/de/mmbbs/Score.java
@@ -0,0 +1,36 @@
+package de.mmbbs;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public class Score {
+	private Integer Punkte;
+	
+	//Konstruktor - score setzen
+	public Score()
+	{
+		Punkte = 10000;
+	}
+	
+	//Punkte hochzählen
+	public void inc()
+	{
+		Punkte = Punkte + 1;
+	}
+	
+	//Punkte zurückgeben
+	public String getScore()
+	{
+		String sPunkte;
+		sPunkte = Punkte.toString();
+		return sPunkte;
+	}
+	
+	//Punkte ausgeben
+	public void paint (Canvas c, Paint p) {
+		c.drawText("Score: " + Punkte, c.getWidth()-100, 10, p);
+	}
+	
+	
+}
