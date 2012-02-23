7034044deafcb674769468b27905266a343f00ab
darkstaple.ggmn@googlemail.com <darkstaple.ggmn@googlemail.com@ac8687c6-834d-e4e5-98ed-2d5b16657450>
2012-02-23 13:59:15 +0000

    Bitmap hinzugefügt, Breite und Höhe der Bitmap wird ausgelesen, Funktion getImage hinzugefügt für Class BasicAnimation

--- trunk/BabylonByBoatMobile/src/de/mmbbs/Basic2dObject.java
+++ trunk/BabylonByBoatMobile/src/de/mmbbs/Basic2dObject.java
@@ -1,44 +1,58 @@
 package de.mmbbs;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.provider.MediaStore.Images;
+import android.widget.ImageView;
 
 public class Basic2dObject {
 	
-	private Images image;
+	//Klassenvaribalen
+	private Bitmap bitmap;
 	private int xPos;
 	private int yPos;
 
-	public Basic2dObject(String ressourcename) {
-		
+	public Basic2dObject(String ressourcesname) {
+		bitmap = BitmapFactory.decodeFile(ressourcesname);
 	}
 	
+	//Gibt Weite als Integer zurück
 	public int getWidth()
 	{
-		return 0;
+		return bitmap.getWidth();
 	}
 	
+	//Gibt hoehe als Integer zurück
 	public int getHeight()
 	{
-		return 0;
+		return bitmap.getHeight();
 	}
-	
-	public boolean hit(int O)
+
+	//Gibt hit als Booliean zurück
+	public boolean hit(Basic2dObject b2dO)
 	{
 		return true; 
 	}
 	
+	//Holt sich die Position des Boots auf der X Achse
 	public int getXPos()
 	{
 		return 0;
 	}
 	
+	//Holt sich die Position des Boots auf der Y Achse
 	public int getYPos()
 	{
 		return 0;
 	}
 	
+	//Paint?
 	public void paint(Canvas c) {
 		
+	}
+	
+	public ImageView getImage(int ressourceID)
+	{
+		return null;
 	}
 }
