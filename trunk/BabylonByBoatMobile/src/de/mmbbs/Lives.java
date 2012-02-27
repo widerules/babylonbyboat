b1917f2124c11641d467fa8057a712c9c27706c3
khakbunn@googlemail.com <khakbunn@googlemail.com@ac8687c6-834d-e4e5-98ed-2d5b16657450>
2012-02-27 21:21:16 +0000

    Alle Leben werden jetzt gezeichnet Position der Leben etwas angepasst

--- trunk/BabylonByBoatMobile/src/de/mmbbs/Lives.java
+++ trunk/BabylonByBoatMobile/src/de/mmbbs/Lives.java
@@ -10,32 +10,44 @@ import android.graphics.Paint;
 
 public class Lives {
 
-    private Integer livecounter;
+    private int livecounter;
     private Bitmap bm;
+    private int bm_width;
 
     // Konstruktor
     public Lives(Context context) {
+    	// Herz Icon zuweisen
         InputStream is = context.getResources().openRawResource(R.drawable.herz);
         BitmapFactory.Options opts = new BitmapFactory.Options();
         opts.inJustDecodeBounds = true;
         this.bm = BitmapFactory.decodeStream(is);
+        // Breite des Icon's speichern
+        this.bm_width = this.bm.getWidth();
 
+        // Am Anfang hat man 3 Leben
         this.livecounter = 3;
     }
 
     // Leben zeichnen
-    @Deprecated
-    public void paint_alt(Canvas g, Paint p) {
-        g.drawText("Lives: " + livecounter.toString(), 10, 10, p);
-    }
-
     public void paint(Canvas g, Paint p) {
-        g.drawBitmap(this.bm, 10, 15, p);
+    	final int top = 15+5; // 15 ist der schwarze Bereich über dem Wasser
+    	int left = 5;
+
+    	for (int i=1;i<=this.livecounter;i++) {
+    		// Bild zeichnen
+        	g.drawBitmap(this.bm, left, top, p);
+        	// Abstand zum nächsten Bild: 5px
+        	left = left + this.bm_width + 5;
+    	}
     }
 
     // Leben um 1 reduzieren
     public void dec() {
         this.livecounter = this.livecounter - 1;
+
+        if (this.livecounter < 0) {
+        	this.livecounter = 0;
+        }
     }
 
     // Leben ausgeben
