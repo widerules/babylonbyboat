6bb4590e9ad685b0dc5604abb2da07b4711b59d2
alexander.maier@dialog-edv.de <alexander.maier@dialog-edv.de@ac8687c6-834d-e4e5-98ed-2d5b16657450>
2012-02-27 18:58:05 +0000

    Hintergrund (Wasser) wird gezeichnet

--- trunk/BabylonByBoatMobile/src/de/mmbbs/GameCanvas.java
+++ trunk/BabylonByBoatMobile/src/de/mmbbs/GameCanvas.java
@@ -23,6 +23,7 @@ public class GameCanvas extends SurfaceView implements ExplosionListener {
     private static final int STATE_LOSTLIVE = 1;
     private static final int STATE_SPIEL = 1;
     private static final int STATE_GAMEOVER = -1;
+    private static final String GAME_TITLE = "BabylonByBoate";
     private Bitmap water;
     private Bitmap wateralpha;
 
@@ -44,14 +45,16 @@ public class GameCanvas extends SurfaceView implements ExplosionListener {
     }
 
     private void initialize() {
+        // Rohbilder in Bitmaps laden
         water = BitmapFactory.decodeResource(getResources(), R.drawable.waterrefl);
+        wateralpha = BitmapFactory.decodeResource(getResources(), R.drawable.waterreflalpha);
+
         ship = new Ship("boat.png");
     }
 
     @Override
     protected void onDraw(Canvas g) {
         // TODO Auto-generated method stub
-
         super.onDraw(g);
         this.render(g);
     }
@@ -60,10 +63,23 @@ public class GameCanvas extends SurfaceView implements ExplosionListener {
         // TODO Auto-generated method stub
         Paint p = new Paint();
         p.setColor(Color.CYAN);
-        c.drawText("Hello Game", 100, 10, p);
+        c.drawText(GAME_TITLE, c.getWidth() / 2 - (GAME_TITLE.length() * 2), 10, p);
+        drawWater(c);
+
         lives.paint(c, p);
         ship.paint(c);
+    }
 
+    private void drawWater(Canvas c) {
+        // Die Wasserbilder müssen noch auf die entsprechende Größe skaliert werden
+        // TODO-: (am-27.02.2012): das Skalieren evtl. in einen Thread auslagern, falls es zu lange dauert
+        //                         aber spätestens, wenn die Animation hinzukommt
+        Bitmap tmpwater = Bitmap.createScaledBitmap(water, c.getWidth(), c.getHeight(), true);
+        Bitmap tmpwateralpha = Bitmap.createScaledBitmap(wateralpha, c.getWidth(), c.getHeight(), true);
+
+        // und zeichnen
+        c.drawBitmap(tmpwater, 0, 15, null);
+        c.drawBitmap(tmpwateralpha, 0, 15, null);
     }
 
     private void run() {
