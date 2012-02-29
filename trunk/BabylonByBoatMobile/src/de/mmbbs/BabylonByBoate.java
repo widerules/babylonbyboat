b3338d0f1cb3b74f7a1f4a3ee627a8240ce3efc0
julianzielke@gmail.com <julianzielke@gmail.com@ac8687c6-834d-e4e5-98ed-2d5b16657450>
2012-02-29 18:40:20 +0000

    Design angepasst AboutMenü hinzugefügt Strings aufgeräumt

--- trunk/BabylonByBoatMobile/src/de/mmbbs/BabylonByBoate.java
+++ trunk/BabylonByBoatMobile/src/de/mmbbs/BabylonByBoate.java
@@ -1,9 +1,13 @@
 package de.mmbbs;
 
+import android.R.string;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -21,16 +25,29 @@ public class BabylonByBoate extends Activity {
         
     }
     
+    //Menü hinzufügen
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.startscreen_menu, menu);
+        return true;    	
+    }
+    
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+	   
+	   AlertDialog.Builder aboutView = new AlertDialog.Builder(this);
+	   aboutView.setTitle(R.string.about_title);
+	   aboutView.setMessage(R.string.about_text);
+	   aboutView.setPositiveButton(R.string.btn_ok, null);
+	   
+	   aboutView.show();
+	   
+	   return true;
+   }
+    
     public void buttonClick(View v) {
     	this.startActivity(new Intent(this,Game.class));
     }
-    
-    /**
-     * Menü mit "about"-Button hinzufügen
-     * 
-     * "AboutView" öffnen (in einem Popup?!)
-     * 
-     * */    
-    
-    
+
 }
