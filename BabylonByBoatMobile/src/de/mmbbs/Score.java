c267f84f2ac13d9bc9b72368e728b22cb0b9a41c
alexander.maier <alexander.maier@dialog-edv.de>
2012-03-01 09:50:46 +0000

    Endlosschleife auskommentiert

--- BabylonByBoatMobile/src/de/mmbbs/Score.java
+++ BabylonByBoatMobile/src/de/mmbbs/Score.java
@@ -4,34 +4,31 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 
 public class Score {
-	private Integer Points;
-	
-	//Konstruktor - score setzen
-	public Score()
-	{
-		this.Points = 0;
-	}
-	
-	//Punkte hochzählen
-	public void inc()
-	{
-		this.Points = this.Points + 1;
-	}
-	
-	//Punkte zurückgeben
-	public String getScore()
-	{
-		String sPoints;
-		sPoints = this.Points.toString();
-		while (sPoints.length() < 5)
-		{
-			sPoints = "0" + this.Points.toString();
-		}
-		return sPoints;
-	}
-	
-	//Punkte ausgeben
-	public void paint(Canvas c, Paint p) {
-		c.drawText("Score: " + getScore(), c.getWidth()-100, 10, p);
-	}	
+
+    private Integer Points;
+
+    //Konstruktor - score setzen
+    public Score() {
+        this.Points = 0;
+    }
+
+    //Punkte hochzählen
+    public void inc() {
+        this.Points = this.Points + 1;
+    }
+
+    //Punkte zurückgeben
+    public String getScore() {
+        String sPoints;
+        sPoints = this.Points.toString();
+        //        while (sPoints.length() < 5) {
+        //            sPoints = "0" + this.Points.toString();
+        //        }
+        return sPoints;
+    }
+
+    //Punkte ausgeben
+    public void paint(Canvas c, Paint p) {
+        c.drawText("Score: " + getScore(), c.getWidth() - 100, 15, p);
+    }
 }
