a520e19aef127f505e08888e6261b932d39cdfb6
lichtkeks <lichtkeks@hotmail.com>
2012-05-03 13:02:31 +0000

    Logo eingefügt

--- BabylonByBoatMobile/src/de/mmbbs/BabylonByBoate.java
+++ BabylonByBoatMobile/src/de/mmbbs/BabylonByBoate.java
@@ -9,7 +9,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.WindowManager.LayoutParams;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class BabylonByBoate extends Activity {
     /** Called when the activity is first created. */
@@ -21,16 +26,15 @@ public class BabylonByBoate extends Activity {
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setTheme(R.style.Theme_Blue);
         setContentView(R.layout.main);
-
-        getWindow().addFlags(LayoutParams.FLAG_KEEP_SCREEN_ON);
+        
     }
     
-    //Men� hinzuf�gen
+    //Menue hinzufuegen
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
     	MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.startscreen_menu, menu);
-        return true;	
+        return true;    	
     }
     
    @Override
@@ -38,9 +42,25 @@ public class BabylonByBoate extends Activity {
 	   
 	   AlertDialog.Builder aboutView = new AlertDialog.Builder(this);
 	   aboutView.setTitle(R.string.about_title);
-	   aboutView.setMessage(R.string.about_text);
-	   aboutView.setPositiveButton(R.string.btn_ok, null);
+	   //aboutView.setMessage(R.string.about_text);
+	   ScrollView sv = new ScrollView(this);
+	     
+	   LinearLayout credits = new LinearLayout(this);
+	   credits.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+	   credits.setOrientation(LinearLayout.VERTICAL);
+	   ImageView img = new ImageView(this);
+	   img.setLayoutParams(new LayoutParams(400, 200));
+	   img.setImageResource(R.drawable.about);
+	   credits.addView(img);
 	   
+	   TextView tv = new TextView(this);
+	   tv.setText(R.string.about_text);
+	   credits.addView(tv);
+	   sv.addView(credits);
+	   aboutView.setView(sv);
+	   aboutView.setPositiveButton(R.string.btn_ok, null);
+	   //aboutView.setIcon(R.drawable.mine);
+	
 	   aboutView.show();
 	   
 	   return true;
