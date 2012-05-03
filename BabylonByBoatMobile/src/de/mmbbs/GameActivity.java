650dfde6caaa5dbc20b7b3cc561931b67d2dab61
alexander.maier <alexander.maier@dialog-edv.de>
2012-05-03 12:56:23 +0000

    Spiel nur noch im Landscape-Modus ausführbar

--- BabylonByBoatMobile/src/de/mmbbs/GameActivity.java
+++ BabylonByBoatMobile/src/de/mmbbs/GameActivity.java
@@ -1,21 +1,28 @@
 package de.mmbbs;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class GameActivity extends Activity {
-	GameManager gm;
-	
-	 @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
-	                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-	        requestWindowFeature(Window.FEATURE_NO_TITLE);
-	        
-	        setContentView(R.layout.game);
-	        gm= new GameManager((GameCanvas) this.findViewById(R.id.gui));
-	    }
+
+    GameManager gm;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.game);
+        gm = new GameManager((GameCanvas) this.findViewById(R.id.gui));
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        // nichts tun
+    }
 }
