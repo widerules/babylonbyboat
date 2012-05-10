3d53f5287b24d468c22db91431c9539219855785
ptuengler <ptuengler@web.de>
2012-05-10 12:53:39 +0000

    Konstruktor geändert(Basic2dAnimation-Operator), statische Start-Methode hinzugefügt

--- BabylonByBoatMobile/src/de/mmbbs/BasicAnimation.java
+++ BabylonByBoatMobile/src/de/mmbbs/BasicAnimation.java
@@ -12,34 +12,55 @@ public class BasicAnimation extends Basic2dObject
 	private int _stepsY;
 	private int _currentStep;
 	private int _resourceName;
-	public BasicAnimation(int resourceName)
+	public BasicAnimation(Basic2dObject object)
 	{
-		super(resourceName);
+		super(resourceName, Context context);
 		_resourceName = resourceName;
 		int xPos = getCurrentPosition().x;
 		int yPos = getCurrentPosition().x;
+		Bitmap bitmap = super.getBitmap();
 		if(resourceName == R.drawable.boat)
 		{
-			_stepsX = 5;
-			paint();
+			_stepsX = 5;			
 		}
-		else
+		else if(resourceName == R.drawable.mine)
 		{
 			_stepsX = 3;
 		}
+	}
+	
+	public static void start()
+	{
+		
 	}
 	public static void reset()
 	{
 		
 	}	
+	private int _stepsY = 0;
+	private float currentsteps = 0;
 	public static void paint()
 	{
-		int currentSteps = 0;
+		if(currentsteps < _stepsX)
+		{
+			
+		}
+		else if(_stepsY = 4)
+		{
+			
+		}
+		else
+		{
+			currentsteps = 0;
+			_stepsY += 1;
+		}
+		
+		currentsteps += 1;
 	}
 	@Override
-	public void paint(Canvas c, Paint p) {
-		// TODO Auto-generated method stub
-		
+	public void paint(Canvas c, Paint p) 
+	{
+		c.drawBitmap();
 	}
 	
 	
